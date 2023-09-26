8aaccc2a5818343e266844d3532c869d9f26e65d
Re-Santos <renata.prog@gmail.com>
2023-09-26 17:36:00 -0300

    template express

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { COURSE_STACK, TCourse } from "./types"
+import { COURSE_STACK, TCourse, TStudent } from "./types"
 
 export const courses: TCourse[] = [
     {
@@ -20,3 +20,18 @@ export const courses: TCourse[] = [
         stack: COURSE_STACK.BACK
     }
 ]
+
+export const students: TStudent [] = [
+    {
+        id: "c001",
+        name: "Amanda",
+        age:12,
+    },
+    {
+        id: "c002",
+        name:"Jéssica",
+        age:10,
+
+    }
+]
+
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { TCourse, TStudent } from './types'
+import { courses, students } from './database'
 
 const app = express()
 
@@ -13,3 +15,64 @@ app.listen(3003, () => {
 app.get('/ping', (req: Request, res: Response) => {
     res.send('Pong!')
 })
+
+app.get('/courses', (req:Request, res:Response)=>{
+    const result: TCourse[] = courses;
+    res.status(200).send(result);
+});
+
+app.get('/courses/search', (req:Request, res:Response)=>{
+    const query: string = req.query.q as string;
+
+    const coursesByName: TCourse[] = courses.filter(course => course.name.toLowerCase() === query.toLocaleLowerCase());
+
+    res.status(200).send(coursesByName);
+});
+
+app.post ('/courses', (req:Request, res:Response)=>{
+    //1° forma de fazer = um pouco longa:
+    // const id: string = req.body.id; //denominei o nome da variável igual a que quero receber = id
+    // const name: string = req.body.name;
+    // const lessons: number = req.body.lessons;
+    // const stack: string = req.body.stack;
+    const {id, name, lessons, stack}: TCourse = req.body; // 2° forma, mas simples
+    const newCourse: TCourse = {
+        id,
+        name,
+        lessons,
+        stack
+    }
+    //Inserindo essa variável dentro do array
+    courses.push(newCourse);
+
+    res.status(201).send ('Curso registrado com sucesso');
+});
+//---------
+
+app.get ('/students', (req:Request, res:Response)=>{
+    const result : TStudent[] = students;
+
+    res.status (200).send(result);
+});
+
+app.get ('/students/search', (req:Request, res:Response)=>{
+    const query: string = req.query.q as string;
+
+    const studentsByName : TStudent[]= students.filter(student=> student.name.toLocaleLowerCase() === query.toLocaleLowerCase());
+
+    res.status(200).send(studentsByName)
+})
+
+app.post ('/students', (req:Request, res:Response)=>{
+
+    const {id, name,age}: TStudent = req.body; // 2° forma, mas simples
+    const newStudent: TStudent = {
+        id,
+        name,
+        age
+    }
+    //Inserindo essa variável dentro do array
+    students.push(newStudent);
+
+    res.status(201).send ('Estudante registrado com sucesso');
+});
